db4eab70b2680b4942d187022f776765a4b21f9b
lydiapf <lydia291@gmail.com>
2020-11-04 12:01:56 -0500

    after tutor

---
+++ EmployeeSQL/Query.sql
@@ -0,0 +1,38 @@
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, sal.salary
+FROM employees AS e
+	LEFT JOIN 
+	salaries as sal
+	ON e.emp_no = sal.emp_no;
+
+--List first name, last name, and hire date for employees who were hired in 1986.
+SELECT first_name, last_name, hire_date
+FROM employees
+	WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+
+--List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM dept_manager AS dm
+	INNER JOIN departments AS d
+		ON dm.dept_no = d.dept_no
+	INNER JOIN employees AS e
+		ON dm.emp_no = e.emp_no;
+
+--List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+	INNER JOIN dept_emp AS de
+		ON e.emp_no = de.emp_no
+	INNER JOIN departments AS d
+		ON d.dept_no = de.dept_no;
+
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
