b9138454bd4cb550b51f4a273e0088ba5e22d3a2
lydiapf <lydia291@gmail.com>
2020-11-04 13:14:30 -0500

    question 7

--- EmployeeSQL/Query.sql
+++ EmployeeSQL/Query.sql
@@ -43,7 +43,13 @@ WHERE d.dept_name = 'Sales';
 		
 
 --List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
-
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+	INNER JOIN dept_emp AS de
+		ON e.emp_no = de.emp_no
+	INNER JOIN departments AS d
+		ON de.dept_no = d.dept_no
+WHERE d.dept_name IN ('Sales', 'Development');
 
 --In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 
